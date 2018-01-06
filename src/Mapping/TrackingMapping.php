b19a34a820da7e4dcee13f7401d4831f688b37a5
Florian Perrot <fperrot@ecedi.fr>
2018-01-06 18:46:23 +0100

    Create service to map query id and field id

---
+++ src/Mapping/TrackingMapping.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * User: Florian Perrot
+ * Date: 06/01/18
+ * Time: 17:46
+ */
+
+namespace App\Mapping;
+
+
+use App\Document\Tracking;
+
+class TrackingMapping
+{
+
+    /**
+     * @var array
+     */
+    private $mapping;
+
+    /**
+     * TrackingMapping constructor.
+     * @param array $mapping
+     */
+    public function __construct(array $mapping)
+    {
+        $this->mapping = $mapping;
+    }
+
+
+    /**
+     * WIP
+     * @param Tracking $tracking
+     * @return array
+     */
+    public function normalize(Tracking $tracking)
+    {
+        return [];
+    }
+
+    /**
+     * @param array $data
+     * @return Tracking
+     */
+    public function denormalize(array $data)
+    {
+        $tracking = new Tracking();
+        $available_keys = $this->getAvailableQuerys();
+        foreach ($data as $key => $value) {
+            if (in_array($key, $available_keys)) {
+                $tracking->set(array_search($key, $this->mapping), $value);
+            }
+        }
+        return $tracking;
+    }
+
+    /**
+     * @return array
+     */
+    private function getAvailablesFields()
+    {
+        return array_keys($this->mapping);
+    }
+
+    /**
+     * @return array
+     */
+    private function getAvailableQuerys()
+    {
+        return array_values($this->mapping);
+    }
+}
